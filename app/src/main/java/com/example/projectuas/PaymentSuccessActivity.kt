934aa5721bd02b663f1eb64216c4323c3bbc81fe
Satrio Agna <satriogemintang@gmail.com>
2024-06-24 14:42:00 +0700

    smol fix

--- app/src/main/java/com/example/projectuas/PaymentSuccessActivity.kt
+++ app/src/main/java/com/example/projectuas/PaymentSuccessActivity.kt
@@ -1,11 +1,23 @@
 package com.example.projectuas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class PaymentSuccessActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_payment_success)
+        val btnNext = findViewById<Button>(R.id.btn_print_e_ticket)
+        btnNext.setOnClickListener {
+            navigateToHome()
+        }
+    }
+
+    private fun navigateToHome() {
+        // Start PaymentActivity
+        val intent = Intent(this, MainScreenActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/projectuas/SeatScreenActivity.kt
+++ app/src/main/java/com/example/projectuas/SeatScreenActivity.kt
@@ -3,10 +3,10 @@ package com.example.projectuas
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.GridLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-
+import androidx.core.content.ContextCompat
+import androidx.gridlayout.widget.GridLayout
 
 class SeatScreenActivity : AppCompatActivity() {
 
@@ -16,38 +16,13 @@ class SeatScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_seat_screen)
 
-        gridLayout = findViewById(R.id.seatGridLayout)
-        setupSeatButtons()
-
+        // Find the next button and set click listener
         val btnNext = findViewById<Button>(R.id.btnNext)
         btnNext.setOnClickListener {
             navigateToPayment()
         }
     }
 
-    private fun setupSeatButtons() {
-        for (i in 0 until gridLayout.childCount) {
-            val button = gridLayout.getChildAt(i) as Button
-            button.setOnClickListener {
-                toggleSeatSelection(button)
-            }
-        }
-    }
-
-    private fun toggleSeatSelection(button: Button) {
-        // Implement logic to toggle seat selection here
-        val currentBackground = button.background.constantState
-        val availableBackground = resources.getDrawable(R.drawable.seat_available, null).constantState
-        val bookedBackground = resources.getDrawable(R.drawable.seat_booked, null).constantState
-
-        if (currentBackground == availableBackground) {
-            button.setBackgroundResource(R.drawable.seat_yourseat)
-        } else if (currentBackground == bookedBackground) {
-            Toast.makeText(this, "Seat already booked", Toast.LENGTH_SHORT).show()
-        } else {
-            button.setBackgroundResource(R.drawable.seat_available)
-        }
-    }
     private fun navigateToPayment() {
         // Start PaymentActivity
         val intent = Intent(this, PaymentActivity::class.java)
